90ce1b61b2f2e4e634d18fcfa149ecead7c96c63
bjli <bjli@itu.dk>
2018-04-26 23:07:30 +0200

    Added lines 52-54 in put(). Not sure if it made any difference...

--- HashPipe.java
+++ HashPipe.java
@@ -17,7 +17,7 @@ public class HashPipe {
 
         stringToReturn = pipe.thePipe[height].str;
         // else {stringToReturn = pipe.thePipe[height].str;} //**********************************Use this instead?
-//********// above line fails: **************************************************
+
 //        //if (str != floorPipe(str).str){return null;} // ie. if the key is NOT in the HashPipe...***************************************************
 //
 //        if (pipe.thePipe[height] == null || pipe == null || pipe.height < height) {return null;}
@@ -49,6 +49,9 @@ public class HashPipe {
         }
 
         for (int i = currentLevel; i >= 0; i--){
+            // if pipeToAdd is AT LEAST AS HIGH as the our current height, and the current pipe we're searching down through points to null at this level
+            // update the references at this level..
+            if(pipeToAdd.height >= currentLevel && currentPipe.thePipe[i] == null){pipeToAdd.thePipe[i] = null;}
 
             // if the pointer found points to 'null' AND were at level '0' - ie. we're at the rightmost pipe at level 0
             if(i == 0 && currentPipe.thePipe[i] == null){currentPipe.thePipe[i] = pipeToAdd;} // 'add' the pipe here, by adding a reference ...
